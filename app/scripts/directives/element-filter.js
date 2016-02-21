2df7b86b9c73b66d3718c2ab072b4ad4dcbd1c8d
Torben Sko <torben.sko@gmail.com>
2016-02-21 16:14:31 +1100

    Made the element toggling CSS based

--- app/scripts/directives/element-filter.js
+++ app/scripts/directives/element-filter.js
@@ -13,19 +13,21 @@ angular.module('websiteApp')
       link: function postLink(scope, element, attrs) {
       	var elements = {};
 
-      	$(attrs.elementFilter).each(function() {
+      	$(attrs.elementFilter+' .filtered').each(function() {
       		var words = $(this).text().replace(/^\s*|\s*$/g, '').split(/[^a-z0-9]+/ig),
       				unique = lodash.uniq(words).join(' ');
       		elements[unique] = $(this);
       	});
 
       	function filterElements() {
-      		var val = new RegExp(element.val(), 'i'),
+      		var val = element.val(),
+      				pattern = new RegExp(val, 'i'),
       				visible = 0;
 
       		lodash.each(elements, function($el, lookup) {
-      			$el.hide();
-      			if ( lookup.match(val) ) { 
+      			$el.removeClass('filtered-hit filtered-miss filtered-default');
+      			
+      			if ( lookup.match(pattern) ) { 
       				if ( visible === 0 ) {
       					$el.siblings('.clearfix').remove();
       				}
@@ -35,8 +37,14 @@ angular.module('websiteApp')
       				if ( visible > 0 && visible % 2 == 0) {
       					$el.before( $('<div class="clearfix visible-sm"></div>') );
       				}
-      				$el.show();
+      				if ( val.length === 0 ) {
+      					$el.addClass('filtered-default');
+      				} else {
+      					$el.addClass('filtered-hit');
+      				}
       				visible++;
+      			} else {
+      				$el.addClass('filtered-miss');
       			}
       		});
       	}
