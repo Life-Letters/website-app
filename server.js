e8ed771627a902406f3d0843f35bdb70dfb8ffe9
Torben Sko <torben.sko@gmail.com>
2016-02-26 20:51:59 +1100

    Used a CORS package

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ var express = require('express');
 var morgan = require('morgan');
 var fs = require('fs');
 var _ = require('lodash');
+var cors = require('cors');
 
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
